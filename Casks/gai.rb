74d0c9e828c9f42b7680fd5de99069a1ec59959e
crown.hg <crown.hg@gmail.com>
2025-02-07 09:55:02 +0800

    upd: version 1.1.0

--- Casks/gai.rb
+++ Casks/gai.rb
@@ -2,14 +2,14 @@
 #                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "gai" do
-  version "1.0.0"
+  version "1.1.0"
   # sha256 "26084e6340ff292e4846ff6d2f68f16cf3d00efa084adfada5141869ff5e62bb"
   sha256 :no_check
 
   url "https://webpath.iche2.com/release/Gai-#{version}-universal.dmg"
   name "Gai"
   desc "Generative-AI Tools For Beginner"
-  homepage "https://webpath.iche2.com/gaidoc/en/"
+  homepage "https://github.com/huanguan1978/pygai"
 
   # Documentation: https://docs.brew.sh/Brew-Livecheck
   livecheck do
