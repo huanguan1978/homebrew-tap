2876df5047111a28eee51d786816b7f6e006a9db
crown.hg <crown.hg@gmail.com>
2025-02-21 15:52:21 +0800

    upd: version 1.1.1

--- Casks/gai.rb
+++ Casks/gai.rb
@@ -2,7 +2,7 @@
 #                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "gai" do
-  version "1.1.0"
+  version "1.1.1"
   # sha256 "26084e6340ff292e4846ff6d2f68f16cf3d00efa084adfada5141869ff5e62bb"
   sha256 :no_check
 
