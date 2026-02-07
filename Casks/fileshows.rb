2ecb34b4aabf3b93ae535ca9e93e3d6315c0bf16
crown.hg <crown.hg@gmail.com>
2026-02-07 14:53:38 +0800

    upd: FileShows 1.0.1

--- Casks/fileshows.rb
+++ Casks/fileshows.rb
@@ -2,7 +2,7 @@
 #                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "fileshows" do
-  version "1.0.0"
+  version "1.0.1"
   # version "1.0.0"  
   # sha256 "c85d6ed784429dced7842626a9bf9fed03d8fbf1b9bb9c899d50d69cfaafb0bd"
   sha256 :no_check
